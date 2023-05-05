566a5a4d1c59c23dfbe742b14a902ea4b1762127
Stewart Lynch <slynch@createchsol.com>
2023-05-05 15:02:53 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Variadic Parameters
+
+This is the completed Project for the YouTube video of the same name
+
+The starter project and video is available in the notes for the video.
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=P9KrmrbWUKo
+" target="_blank"><img src="http://img.youtube.com/vi/P9KrmrbWUKo/0.jpg" 
+alt="NavigationBar Styling in SwiftUI" width="480" height="360" border="10" /></a>
+
---
+++ VariadicParameters.playground/Pages/DebugPrint.xcplaygroundpage/Contents.swift
@@ -0,0 +1,79 @@
+/*:
+ [< Previous](@previous)                    [Home](Introduction)
+ ## Debug Print
+ - Callout(Debug Print): Create a new print function that extends the usefulness of the variadic print function.
+*/
+import Foundation
+
+code(for: "Namespace Debug print") {
+    // Recreate the print function in an Enum namespace
+    enum Debug {
+        static func print(
+            _ items: Any...,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(items, separator: separator, terminator: terminator)
+        }
+    }
+    Debug.print(1, 2, 3)
+}
+
+code(for: "Emoji previx") {
+    // add a number of emooji cases to the enum and use as a prefix for the print
+    enum Debug: String {
+        case pin = "📌"
+        case star = "⭐️"
+        case note = "📝"
+        case globe = "🌎"
+        static func print(
+            _ items: Any...,
+            type: Debug,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(type.rawValue,
+                        items.map {"\($0)"}.joined(separator: separator),
+                        separator: separator,
+                        terminator: terminator)
+        }
+    }
+    Debug.print(1, 2, 3, type: .pin)
+}
+
+code(for: "Debug Identifiers") {
+    // Extend the function to allow for an optional set of Swift Keyword debug identifiers
+    enum Debug: String {
+        case pin = "📌"
+        case star = "⭐️"
+        case note = "📝"
+        case globe = "🌎"
+        static func print(
+            _ items: Any...,
+            type: Debug,
+            extended: Bool = false,
+            separator: String = " ",
+            terminator: String = "\n"
+        ) {
+            Swift.print(type.rawValue,
+                        items.map {"\($0)"}.joined(separator: separator),
+                        separator: separator,
+                        terminator: terminator)
+            if extended {
+                Swift.print(
+                """
+                \(#fileID)
+                function: \(#function)
+                line: \(#line)
+                -----------------------------------------
+                """
+                )
+            }
+        }
+    }
+    Debug.print(1,2,3, type: .note)
+    Debug.print(1,2,3, type: .star, extended: true)
+}
+/*:
+ [< Previous](@previous)                    [Home](Introduction)
+*/
